bc572dcf3bbfe81795ea16008703b107dee22ee3
Andy Collins <anthonius.bruce@gmail.com>
2025-02-08 13:59:11 -0500

    Changed error page to something less default

--- Program.cs
+++ Program.cs
@@ -14,15 +14,16 @@ var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
 var secret = await client.GetSecretAsync("appsettingscipher");
 
 var builder = WebApplication.CreateBuilder(args);
-if (secret != null)
-{ 
-    // Read the connection string from the appsettings.json file
-    builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-    // Get HostingEnvironment
-    var env = builder.Environment;
-    builder.Configuration.AddJsonFile($"appsettings{env.EnvironmentName}.json", optional: true);
-    builder.Configuration.Decrypt(secret.Value.Value);
-}
+
+if (secret == null)
+    throw new InvalidOperationException();
+
+// Read the connection string from the appsettings.json file
+builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+// Get HostingEnvironment
+var env = builder.Environment;
+builder.Configuration.AddJsonFile($"appsettings{env.EnvironmentName}.json", optional: true);
+builder.Configuration.Decrypt(secret.Value.Value);
 
 builder.Configuration.AddEnvironmentVariables().AddUserSecrets(Assembly.GetExecutingAssembly(), true);
 
@@ -59,7 +60,7 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
         };
     });
 
-if (secret != null && builder.Environment.IsDevelopment())
+if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<ShirtStormDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 }
