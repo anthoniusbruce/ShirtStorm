b6f73e66d9568a7a648d9ccb89e35ff2f20bfb0a
Andy Collins <anthonius.bruce@gmail.com>
2025-03-05 13:50:01 -0500

    updated to component for preamble

--- ShirtStormMvc/Controllers/HomeController.cs
+++ ShirtStormMvc/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ShirtStormCommon.Models;
---
+++ ShirtStormMvc/ViewComponents/PreambleViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShirtStormMvc.ViewComponents
+{
+    public class PreambleViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
