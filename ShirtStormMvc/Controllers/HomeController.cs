eb64a9cd9789fe66917aac1a79417470b49023a3
Andy Collins <anthonius.bruce@gmail.com>
2025-03-09 13:52:12 -0400

    added antiforgery validation

--- ShirtStormMvc/Controllers/HomeController.cs
+++ ShirtStormMvc/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using ShirtStormMvc.Models;
 
 namespace ShirtStormMvc.Controllers;
 
+[AutoValidateAntiforgeryToken]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
